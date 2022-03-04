38e1c150faa21026666ef485b1e55afdeb082b74
clotildebeckerlamoureux <clobecker77@gmail.com>
2022-03-04 12:09:32 +0100

    upload étape 1

--- sapin-exo-indiv.js
+++ sapin-exo-indiv.js
@@ -1,22 +1,14 @@
 let star = ["&nbsp;&nbsp;","&nbsp;","+","&nbsp;","&nbsp;"]
 let firstRow = ["&nbsp;&nbsp;","/","*","\\","&nbsp;"]
 let secondRow = ["/","*","*","*","\\"]
-let sapin1_1 = [star.join(' ') + '<br />', firstRow.join(' ') + '<br />', secondRow.join(' ') + '<br />']
 
-function sapin1 () {
-    for (var i = 0; i < sapin1_1[star.length]; i++) { 
-        document.write(sapin1_1[star[i]])
-    }
-    
-    for (var i = 0; i < sapin1_1[firstRow.length]; i++) { 
-        document.write(sapin1_1[firstRow[i]])
-    }
-    
-    for (var i = 0; i < sapin1_1[secondRow.length]; i++) { 
-        document.write(sapin1_1[secondRow[i]])
+function sapin1 (call) {
+    call = prompt("Choisis un nombre entre 1 et 5")
+    if (call == 1) {
+        let sapin1_1 = [star.join(' ') + '<br />', firstRow.join(' ') + '<br />', secondRow.join(' ') + '<br />']
+        document.write(sapin1_1.join(' '))
     }
 }
 
-//sapin1()
+sapin1()
 
-document.write(sapin1_1.join(' '))
