63631b474893dab66e6674f1c58b6d9399ed8873
clotildebeckerlamoureux <clobecker77@gmail.com>
2022-03-17 11:05:52 +0100

    upload étape 4

--- allumettes.js
+++ allumettes.js
@@ -1,21 +1,29 @@
 let match = 50
+let player1
+let player2
+let game = true
 
 function matchToRemove() {
     while (match > 0) {
-        let player1 = parseInt(prompt("Choisis un nombre d'allumettes à retirer \nNombre d'allumettes restantes : " + match))
-        if (player1 < 1 || player1 > 6) {
+        player()
+        if (player1 < 1 || player1 > 6 || player2 < 1 || player2 > 6) {
             alert("Choisis un nombre entre 1 et 6")
             matchToRemove()
         }
-        else if (match < player1) {
+        else if (match < player1 || match < player2) {
             //let erreur = 
             alert("Choisis un nombre plus petit")
             matchToRemove()
         } 
         else
         //console.log(player1)
-        match = match - player1
-        console.log(match) 
+        if (game == false) {
+            match = match - player1
+            console.log(match)
+        } else {
+            match = match - player2
+            console.log(match)
+        }
     }
     if (match === 0) {
         //let victoire = 
@@ -23,6 +31,17 @@ function matchToRemove() {
         match = 50
         matchToRemove()
         
+    }
+}
+
+function player(){
+    if (game == true) {
+        player1 = parseInt(prompt("Joueur 1 \nChoisis un nombre d'allumettes à retirer \nNombre d'allumettes restantes : " + match))
+        game = false
+    } 
+    else {
+        player2 = parseInt(prompt("Joueur 2 \nChoisis un nombre d'allumettes à retirer \nNombre d'allumettes restantes : " + match))
+        game = true
     }
 }
 
